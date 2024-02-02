8789b93854eeda9702671c9ab53cfd36ee622e11
Jessicarrr <JWP01993@gmail.com>
2024-02-02 21:17:39 +1000

    add harmony patches for arbitrary editing of pawn opinions
    - Added two harmony patches to arbitrarily edit pawn relations with one another.
    - Added a flat -25 reduction to all pawn relations
    - Added an explanation to this -25 reduction as "Had an arbitrary memory".
    - This is for testing purposes - in future this will be for ChatGPT or other LLMs being able to inject their own memories of conversations directly to the pawns.

---
+++ Source/TalkToPawns/AiConversations/HarmonyPatches/Pawn_RelationsTracker_OpinionExplanation.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AiConversations.HarmonyPatches
+{
+    [HarmonyPatch(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.OpinionExplanation))]
+    public class Pawn_RelationsTracker_OpinionExplanation
+    {
+        public static void Postfix(Pawn_RelationsTracker __instance, ref string __result, Pawn other)
+        {
+            var stringBuilder = new StringBuilder(__result + "\n");
+            stringBuilder.AppendLine("- Had an arbitrary memory -25");
+            __result = stringBuilder.ToString();
+        }
+    }
+}
---
+++ Source/TalkToPawns/AiConversations/HarmonyPatches/Pawn_RelationsTracker_OpinionOf.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace AiConversations.HarmonyPatches
+{
+    [HarmonyPatch(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.OpinionOf))]
+    public class Pawn_RelationsTracker_OpinionOf
+    {
+        public static void Postfix(Pawn_RelationsTracker __instance, ref int __result, Pawn other)
+        {
+            // Access the 'pawn' private field using reflection
+            Pawn pawn = (Pawn)AccessTools.Field(typeof(Pawn_RelationsTracker), "pawn").GetValue(__instance);
+
+            __result = __result - 25; // Arbitrary decrease in relationship score
+
+            // Ensure the result is not positive if the pawn is hostile to the other
+            if (__result > 0 && pawn.HostileTo(other))
+            {
+                __result = 0;
+            }
+
+            // Clamp the result to be within the game's expected range for opinion scores
+            __result = Mathf.Clamp(__result, -100, 100);
+        }
+    }
+}
